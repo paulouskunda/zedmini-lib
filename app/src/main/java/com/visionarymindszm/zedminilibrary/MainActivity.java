d5e0df82c898c95385103abfc9f9f6e99a98f574
Paul Kunda <paulouskunda@gmail.com>
2021-03-22 15:12:46 +0200

    Second Push

--- app/src/main/java/com/visionarymindszm/zedminilibrary/MainActivity.java
+++ app/src/main/java/com/visionarymindszm/zedminilibrary/MainActivity.java
@@ -3,12 +3,23 @@ package com.visionarymindszm.zedminilibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText pickDistrictEditText = findViewById(R.id.pickDistrictEditText);
+        EditText pickProvinceEditText = findViewById(R.id.pickProvinceEditText);
+
+        pickProvinceEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
