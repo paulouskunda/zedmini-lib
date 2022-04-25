4ae8fcf1fc2bfb2193b1026e74fad6dbc3678605
Paul Kunda <=>
2022-04-25 14:28:03 +0200

    Changes to country Gradle

--- ZedEditOptions/src/main/java/com/visionarymindszm/zededitoptions/CountriesUtils.kt
+++ ZedEditOptions/src/main/java/com/visionarymindszm/zededitoptions/CountriesUtils.kt
@@ -1,5 +1,6 @@
 package com.visionarymindszm.zededitoptions
 
+import android.util.Log
 import com.visionarymindszm.zededitoptions.model.Countries
 import com.visionarymindszm.zededitoptions.services.ApiClient
 import com.visionarymindszm.zededitoptions.services.ApiService
@@ -26,12 +27,43 @@ class CountriesUtils {
                         countries.add(count.name!!)
                     }
                 }
+
+                Log.i(TAG, "Countries size ${countries.size}")
+                Log.i(TAG, "Country at Zero ${countries[0]}")
             }
 
             override fun onFailure(call: Call<List<Countries>>, t: Throwable) {
-
+                    Log.e(TAG, "We encountered error ${t.message}")
             }
 
         })
+    }
+
+    fun getAllCountries(){
+        val apiService = ApiClient.client!!.create(ApiService::class.java)
+        val apiCall = apiService.getAllCountryNamesOnly()
+        apiCall.enqueue(object : Callback<List<Countries>>{
+            override fun onResponse(
+                call: Call<List<Countries>>,
+                response: Response<List<Countries>>
+            ) {
+                countries = ArrayList()
+                val countriesResponse = response.body()
+                countriesResponse?.forEach { count ->
+                    run {
+                        countries.add(count.name!!)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Countries>>, t: Throwable) {
+                Log.e(TAG, "We encountered error ${t.message}")
+            }
+
+        })
+    }
+
+    companion object{
+        const val TAG = "CountriesUtil";
     }
 }
