bd5e32f1e374825f9463237009c693381f57fb73
Paul Kunda <pkunda@visionarymindszm.com>
2022-11-05 13:33:53 +0200

    NRC Formatter Test

--- app/src/main/java/com/visionarymindszm/zedminilibrary/MainActivity.java
+++ app/src/main/java/com/visionarymindszm/zedminilibrary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.visionarymindszm.zedminilibrary;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -8,25 +9,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.visionarymindszm.zededitoptions.ZedUtils;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText pickDistrictEditText = findViewById(R.id.pickDistrictEditText);
-        final EditText pickProvinceEditText = findViewById(R.id.pickProvinceEditText);
-//        Toast.makeText(this, new CountriesUtils(), Toast.LENGTH_LONG).show();
-        pickProvinceEditText.setOnClickListener(view -> ZedUtils.showDialog("Select Province", MainActivity.this, ZedUtils.PROVINCES, pickProvinceEditText));
+        EditText pickDistrictEditText = findViewById(R.id.pickDistrictEditText);
+        EditText pickProvinceEditText = findViewById(R.id.pickProvinceEditText);
+        EditText nrcNumber = findViewById(R.id.nrcNumber);
+        Button validateFields = findViewById(R.id.validateFields);
+        pickProvinceEditText.setOnClickListener(view -> ZedUtils.INSTANCE.showDialog("Select Province", MainActivity.this, ZedUtils.INSTANCE.getPROVINCES(), pickProvinceEditText));
 
         pickDistrictEditText.setOnClickListener(view -> {
             if (pickProvinceEditText.getText().toString().isEmpty()) {
                 pickDistrictEditText.setText("Please select a province");
             }else{
-                ZedUtils.showDialog("Select District From "+pickProvinceEditText.getText().toString(), MainActivity.this,
-                        ZedUtils.getDistrictArray(pickProvinceEditText.getText().toString()), pickDistrictEditText);
+                ZedUtils.INSTANCE.showDialog("Select District From "+pickProvinceEditText.getText().toString(), MainActivity.this,
+                        ZedUtils.INSTANCE.getDistrictArray(pickProvinceEditText.getText().toString()), pickDistrictEditText);
             }
-
         });
+
+        ZedUtils.INSTANCE.nrcValidation(nrcNumber);
+
+        EditText[] editTexts = {pickDistrictEditText, pickProvinceEditText, nrcNumber};
+        validateFields.setOnClickListener(v -> {
+            boolean isAllCool =  ZedUtils.INSTANCE.validateEditFields(getResources(), editTexts);
+            if (isAllCool){
+                Toast.makeText(getApplicationContext(), "All Good", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
     }
 }
